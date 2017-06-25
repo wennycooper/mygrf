0f769638eb81176cbc228da955988f64568f6dd4
wennycooper <kevin.kuei.0321@gmail.com>
2017-06-25 08:45:52 +0800

    add try and exception on tf

--- scripts/trainKmeans.py
+++ scripts/trainKmeans.py
@@ -38,11 +38,48 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         now = rospy.Time.now()
         duration = rospy.Duration(1.0)
+
+        try:
+
+            listener.waitForTransform(TORSO, HEAD, now, duration)
+            (head, rot) = listener.lookupTransform(TORSO, HEAD, now)
+            listener.waitForTransform(TORSO, NECK, now, duration)
+            (neck, rot) = listener.lookupTransform(TORSO, NECK, now)
+            listener.waitForTransform(TORSO, TORSO, now, duration)
+            (torso, rot) = listener.lookupTransform(TORSO, TORSO, now)
+
+            listener.waitForTransform(TORSO, LEFT_SHOULDER, now, duration)
+            (left_shoulder, rot) = listener.lookupTransform(TORSO, LEFT_SHOULDER, now)
+            listener.waitForTransform(TORSO, LEFT_ELBOW, now, duration)
+            (left_elbow, rot) = listener.lookupTransform(TORSO, LEFT_ELBOW, now)
+            listener.waitForTransform(TORSO, LEFT_HAND, now, duration)
+            (left_hand, rot) = listener.lookupTransform(TORSO, LEFT_HAND, now)
+
+            listener.waitForTransform(TORSO, RIGHT_SHOULDER, now, duration)
+            (right_shoulder, rot) = listener.lookupTransform(TORSO, RIGHT_SHOULDER, now)
+            listener.waitForTransform(TORSO, RIGHT_ELBOW, now, duration)
+            (right_elbow, rot) = listener.lookupTransform(TORSO, RIGHT_ELBOW, now)
+            listener.waitForTransform(TORSO, RIGHT_HAND, now, duration)
+            (right_hand, rot) = listener.lookupTransform(TORSO, RIGHT_HAND, now)
+
+            listener.waitForTransform(TORSO, LEFT_HIP, now, duration)
+            (left_hip, rot) = listener.lookupTransform(TORSO, LEFT_HIP, now)
+            listener.waitForTransform(TORSO, LEFT_KNEE, now, duration)
+            (left_knee, rot) = listener.lookupTransform(TORSO, LEFT_KNEE, now)
+            listener.waitForTransform(TORSO, LEFT_FOOT, now, duration)
+            (left_foot, rot) = listener.lookupTransform(TORSO, LEFT_FOOT, now)
+
+            listener.waitForTransform(TORSO, RIGHT_HIP, now, duration)
+            (right_hip, rot) = listener.lookupTransform(TORSO, RIGHT_HIP, now)
+            listener.waitForTransform(TORSO, RIGHT_KNEE, now, duration)
+            (right_knee, rot) = listener.lookupTransform(TORSO, RIGHT_KNEE, now)
+            listener.waitForTransform(TORSO, RIGHT_FOOT, now, duration)
+            (right_foot, rot) = listener.lookupTransform(TORSO, RIGHT_FOOT, now)
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
         
-        listener.waitForTransform(TORSO, RIGHT_HAND, now, duration)
-        (trans, rot) = listener.lookupTransform(TORSO, RIGHT_HAND, now)
-        
-        print(trans)
+        print(right_hand)
         
         rate.sleep()
 
